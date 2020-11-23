961c792452deeaf51e36b3cafde5d1accebb0b75
shahoul95 <shahoul95@gmail.com>
2020-11-23 14:08:22 +0100

    sonars

---
+++ Migrations/20201123125708_djibsonmigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace foots.Migrations
+{
+    public partial class djibsonmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
